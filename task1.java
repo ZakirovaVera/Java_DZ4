377f741f58c4759705e19a0daf1b9a650308e951
ZakirovaVera <zakirovavk@gmail.com>
2022-12-03 23:46:31 +0500

    дано задач

---
+++ task1.java
@@ -0,0 +1,8 @@
+// Пусть дан LinkedList с несколькими элементами. Реализуйте метод, 
+// который вернет “перевернутый” список.
+
+public class task1 {
+    public static void main(String[] args) {
+        
+    }
+}
